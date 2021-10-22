2a836820500e5c1b00dd24fe96399a0072204ca4
Mukul <74971960+mukul-raj@users.noreply.github.com>
2021-10-23 00:00:44 +0530

    initial commit

---
+++ assets/colors/colors.js
@@ -0,0 +1,10 @@
+const colors = {
+    redDark: "#8E1F51",
+    red: "#E54059",
+    redLight: "#DE837F",
+    blue : "#02A1FF",
+    blueLight: "#63DAEE",
+    orange: "#FDB36A",
+}
+
+export default colors;
